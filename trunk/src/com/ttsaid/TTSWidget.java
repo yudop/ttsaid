46aa238b93dccade595d2670728f83df7beae42f
carlosbar@gmail.com <carlosbar@gmail.com@845f0434-9599-a32b-f699-e6e2c56f4ca7>
2012-10-19 14:33:07 +0000

    flash camera led on new sms/call

--- trunk/src/com/ttsaid/TTSWidget.java
+++ trunk/src/com/ttsaid/TTSWidget.java
@@ -28,11 +28,14 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class TTSWidget extends AppWidgetProvider {
 	public	static	final	String					PREFS_DB = "com.ttsaid.prefs.db";
@@ -74,11 +77,14 @@ public class TTSWidget extends AppWidgetProvider {
 				}
 			} else if (TelephonyManager.ACTION_PHONE_STATE_CHANGED.equals(intent.getAction())) {
 				if (intent.hasExtra(TelephonyManager.EXTRA_STATE)) {
-					if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING) && prefs.getBoolean("SET_CALLER_ID", false)) {
-						final Intent si = new Intent(context,TTSService.class);
-						si.setAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
-						si.putExtra("phoneNumber",intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
-						context.startService(si);
+					if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+						if(prefs.getBoolean("PHONE_FLASH", true)) flashLight(context);
+						if(prefs.getBoolean("SET_CALLER_ID", false)) {
+							final Intent si = new Intent(context,TTSService.class);
+							si.setAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
+							si.putExtra("phoneNumber",intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+							context.startService(si);
+						}
 					}
 				}
 			} else if (PLAY_TIME.equals(intent.getAction()) || PLAY_AND_ENQUEUE.equals(intent.getAction()) || Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
@@ -91,25 +97,65 @@ public class TTSWidget extends AppWidgetProvider {
 					si.putExtra("screenEvent",true);
 				}
 				context.startService(si);
-			} else if(SMS_RECEIVED_ACTION.equals(intent.getAction()) && prefs.getBoolean("SET_SMS_RECEIVE", false)) {
-				Bundle bundle = intent.getExtras();
-				if(bundle == null) {
-					return;
-				}
-				Object[] pdus = (Object[])bundle.get("pdus");
-				final SmsMessage[] messages = new SmsMessage[pdus.length];
-				for (int i = 0; i < pdus.length; i++) {
-					messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-				}
-				if (messages.length > -1) {
-					final Intent si = new Intent(context,TTSService.class);
-					si.setAction(SMS_RECEIVED_ACTION);
-					si.putExtra("message",messages[0].getMessageBody());
-					si.putExtra("phoneNumber",messages[0].getOriginatingAddress());
-					context.startService(si);
+			} else if(SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+				if(prefs.getBoolean("SMS_FLASH", true)) flashLight(context);
+				if(prefs.getBoolean("SET_SMS_RECEIVE", false)) {
+					Bundle bundle = intent.getExtras();
+					if(bundle == null) {
+						return;
+					}
+					Object[] pdus = (Object[])bundle.get("pdus");
+					final SmsMessage[] messages = new SmsMessage[pdus.length];
+					for (int i = 0; i < pdus.length; i++) {
+						messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+					}
+					if (messages.length > -1) {
+						final Intent si = new Intent(context,TTSService.class);
+						si.setAction(SMS_RECEIVED_ACTION);
+						si.putExtra("message",messages[0].getMessageBody());
+						si.putExtra("phoneNumber",messages[0].getOriginatingAddress());
+						context.startService(si);
+					}
 				}
 			}
 		}
+	}
+	
+	/**
+	 * flash the camera light
+	 */
+	public void flashLight(Context context) {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				for(int i=0;i < 3;i++) {
+					try {
+						Camera mCamera = Camera.open();
+						Parameters params = mCamera.getParameters();
+						for(int x=0;x < 3;x++) {
+				            params.setFlashMode( Parameters.FLASH_MODE_TORCH);
+				            mCamera.setParameters(params);
+				            mCamera.startPreview();
+				            try {
+				            	Thread.sleep(10);
+			                } catch (InterruptedException e) {
+			                	e.printStackTrace();
+			                }
+				            params.setFlashMode( Parameters.FLASH_MODE_OFF);
+							mCamera.setParameters(params);
+							mCamera.stopPreview();
+						}
+						mCamera.release();
+					} catch (Exception e) {
+					}
+		            try {
+		            	Thread.sleep(50);
+	                } catch (InterruptedException e) {
+	                	e.printStackTrace();
+	                }
+				}
+			}
+		});
+		t.start();
 	}
 
 	@Override
